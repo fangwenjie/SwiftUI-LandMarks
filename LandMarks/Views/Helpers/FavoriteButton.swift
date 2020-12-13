1c64090afeb2e60989ee4d78db354b2c23c7dc04
fangwenjie <colawalnut@gmail.com>
2020-12-14 00:53:47 +0800

    Create a Favorite Button for each landmark

---
+++ LandMarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  LandMarks
+//
+//  Created by 方文杰 on 2020/12/14.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(false))
+    }
+}
